4a6c99885be31d611c76af95f3181cfd6593d7f9
lm2a <mario.lamenza@gmail.com>
2025-06-25 10:33:17 +0200

    Sexto commit (Dia 3). Taco -> Persistencia usando PreparedStatementCreatorFactory y KeyHolder

---
+++ src/main/java/com/lm2a/tacoonline/data/TacoRepository.java
@@ -0,0 +1,7 @@
+package com.lm2a.tacoonline.data;
+
+import com.lm2a.tacoonline.model.Taco;
+
+public interface TacoRepository {
+    Taco save(Taco design);
+}
---
+++ src/main/java/com/lm2a/tacoonline/web/IngredientByIdConverter.java
@@ -0,0 +1,19 @@
+package com.lm2a.tacoonline.web;
+
+import com.lm2a.tacoonline.data.IngredientRepository;
+import com.lm2a.tacoonline.model.Ingredient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class IngredientByIdConverter implements Converter<String, Ingredient> {
+
+    final IngredientRepository ingredientRepository;
+
+    @Override
+    public Ingredient convert(String source) {
+        return ingredientRepository.findOne(source);
+    }
+}
